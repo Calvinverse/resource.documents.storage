10f6970330d1cbb9fcfed57a7adc6c237443ef28
Petrik van der Velde <petrikvandervelde@gmail.com>
2019-02-10 20:12:39 +1300

    Stick the list of master hosts in another file
    references #1

--- src/cookbooks/resource_documents_storage/recipes/elasticsearch.rb
+++ src/cookbooks/resource_documents_storage/recipes/elasticsearch.rb
@@ -304,6 +304,7 @@ file "#{consul_template_template_path}/#{elasticsearch_config_script_template_fi
     FLAG=$(cat #{flag_config})
     if [ "$FLAG" = "NotInitialized" ]; then
         echo "Write the elasticsearch configuration script ..."
+
         cat <<'EOT' > #{elasticsearch_config_path}/elasticsearch.yml
     cluster.name: "{{ key "config/services/consul/datacenter" }}"
     node.name: ${HOSTNAME}
@@ -316,13 +317,7 @@ file "#{consul_template_template_path}/#{elasticsearch_config_script_template_fi
     http.port: #{http_port}
     transport.tcp.port: #{discovery_port}
 
-    {{ $services := service "#{consul_service_tag}.#{consul_service_name}" }}
-    {{ if containsAny $services }}
-    discovery.zen.ping.unicast.hosts:
-    {{ range $services }}
-      - {{ .Name }}:#{discovery_port}
-    {{ end }}
-    {{ end }}
+    discovery.zen.host_provider: file
     discovery.zen.minimum_master_nodes: {{ key "config/services/documents/masters" }}
     EOT
 
@@ -396,6 +391,93 @@ file "#{consul_template_config_path}/elasticsearch_config.hcl" do
       # return within 30s (configurable), and it must have a successful exit code.
       # Consul Template is not a replacement for a process monitor or init system.
       command = "sh #{elasticsearch_config_script_file}"
+
+      # This is the maximum amount of time to wait for the optional command to
+      # return. Default is 30s.
+      command_timeout = "45s"
+
+      # Exit with an error when accessing a struct or map field/key that does not
+      # exist. The default behavior will print "<no value>" when accessing a field
+      # that does not exist. It is highly recommended you set this to "true" when
+      # retrieving secrets from Vault.
+      error_on_missing_key = false
+
+      # This is the permission to render the file. If this option is left
+      # unspecified, Consul Template will attempt to match the permissions of the
+      # file that already exists at the destination path. If no file exists at that
+      # path, the permissions are 0644.
+      perms = 0550
+
+      # This option backs up the previously rendered template at the destination
+      # path before writing a new one. It keeps exactly one backup. This option is
+      # useful for preventing accidental changes to the data without having a
+      # rollback strategy.
+      backup = true
+
+      # These are the delimiters to use in the template. The default is "{{" and
+      # "}}", but for some templates, it may be easier to use a different delimiter
+      # that does not conflict with the output file itself.
+      left_delimiter  = "{{"
+      right_delimiter = "}}"
+
+      # This is the `minimum(:maximum)` to wait before rendering a new template to
+      # disk and triggering a command, separated by a colon (`:`). If the optional
+      # maximum value is omitted, it is assumed to be 4x the required minimum value.
+      # This is a numeric time with a unit suffix ("5s"). There is no default value.
+      # The wait value for a template takes precedence over any globally-configured
+      # wait.
+      wait {
+        min = "2s"
+        max = "10s"
+      }
+    }
+  HCL
+  group 'root'
+  mode '0550'
+  owner 'root'
+end
+
+elasticsearch_hosts_template_file = 'elasticsearch_hosts.ctmpl'
+file "#{consul_template_template_path}/#{elasticsearch_hosts_template_file}" do
+  action :create
+  content <<~CONF
+    {{ $services := service "#{consul_service_tag}.#{consul_service_name}" }}
+    {{ range $services }}
+      - {{ .Name }}:#{discovery_port}
+    {{ end }}
+  CONF
+  group 'root'
+  mode '0550'
+  owner 'root'
+end
+
+elasticsearch_hosts_file = "#{node['elasticsearch']['path']['config']}/unicast_hosts.txt"
+file "#{consul_template_config_path}/elasticsearch_hosts.hcl" do
+  action :create
+  content <<~HCL
+    # This block defines the configuration for a template. Unlike other blocks,
+    # this block may be specified multiple times to configure multiple templates.
+    # It is also possible to configure templates via the CLI directly.
+    template {
+      # This is the source file on disk to use as the input template. This is often
+      # called the "Consul Template template". This option is required if not using
+      # the `contents` option.
+      source = "#{consul_template_template_path}/#{elasticsearch_hosts_template_file}"
+
+      # This is the destination path on disk where the source template will render.
+      # If the parent directories do not exist, Consul Template will attempt to
+      # create them, unless create_dest_dirs is false.
+      destination = "#{elasticsearch_hosts_file}"
+
+      # This options tells Consul Template to create the parent directories of the
+      # destination path if they do not exist. The default value is true.
+      create_dest_dirs = false
+
+      # This is the optional command to run when the template is rendered. The
+      # command will only run if the resulting template changes. The command must
+      # return within 30s (configurable), and it must have a successful exit code.
+      # Consul Template is not a replacement for a process monitor or init system.
+      command = ""
 
       # This is the maximum amount of time to wait for the optional command to
       # return. Default is 30s.
--- src/cookbooks/resource_documents_storage/spec/elasticsearch_spec.rb
+++ src/cookbooks/resource_documents_storage/spec/elasticsearch_spec.rb
@@ -219,6 +219,7 @@ describe 'resource_documents_storage::elasticsearch' do
       FLAG=$(cat /var/log/elasticsearch_config.log)
       if [ "$FLAG" = "NotInitialized" ]; then
           echo "Write the elasticsearch configuration script ..."
+
           cat <<'EOT' > /etc/elasticsearch/elasticsearch.yml
       cluster.name: "{{ key "config/services/consul/datacenter" }}"
       node.name: ${HOSTNAME}
@@ -231,14 +232,7 @@ describe 'resource_documents_storage::elasticsearch' do
       http.port: 9200
       transport.tcp.port: 9300
 
-
-      {{ $services := service "http.documents" }}
-      {{ if containsAny $services }}
-      discovery.zen.ping.unicast.hosts:
-      {{ range $services }}
-        - {{ .Name }}:9300
-      {{ end }}
-      {{ end }}
+      discovery.zen.host_provider: file
       discovery.zen.minimum_master_nodes: {{ key "config/services/documents/masters" }}
       EOT
 
@@ -358,6 +352,97 @@ describe 'resource_documents_storage::elasticsearch' do
     it 'creates elasticsearch_config.hcl in the consul-template template directory' do
       expect(chef_run).to create_file('/etc/consul-template.d/conf/elasticsearch_config.hcl')
         .with_content(consul_template_elasticsearch_config_content)
+        .with(
+          group: 'root',
+          owner: 'root',
+          mode: '0550'
+        )
+    end
+
+    elasticsearch_hosts_template_content = <<~CONF
+      {{ $services := service "http.documents" }}
+      {{ range $services }}
+        - {{ .Name }}:9300
+      {{ end }}
+    CONF
+    it 'creates ElasticSearch hosts template file in the consul-template template directory' do
+      expect(chef_run).to create_file('/etc/consul-template.d/templates/elasticsearch_hosts.ctmpl')
+        .with_content(elasticsearch_hosts_template_content)
+        .with(
+          group: 'root',
+          owner: 'root',
+          mode: '0550'
+        )
+    end
+
+    consul_template_elasticsearch_hosts_content = <<~CONF
+      # This block defines the configuration for a template. Unlike other blocks,
+      # this block may be specified multiple times to configure multiple templates.
+      # It is also possible to configure templates via the CLI directly.
+      template {
+        # This is the source file on disk to use as the input template. This is often
+        # called the "Consul Template template". This option is required if not using
+        # the `contents` option.
+        source = "/etc/consul-template.d/templates/elasticsearch_hosts.ctmpl"
+
+        # This is the destination path on disk where the source template will render.
+        # If the parent directories do not exist, Consul Template will attempt to
+        # create them, unless create_dest_dirs is false.
+        destination = "/etc/elasticsearch/unicast_hosts.txt"
+
+        # This options tells Consul Template to create the parent directories of the
+        # destination path if they do not exist. The default value is true.
+        create_dest_dirs = false
+
+        # This is the optional command to run when the template is rendered. The
+        # command will only run if the resulting template changes. The command must
+        # return within 30s (configurable), and it must have a successful exit code.
+        # Consul Template is not a replacement for a process monitor or init system.
+        command = ""
+
+        # This is the maximum amount of time to wait for the optional command to
+        # return. Default is 30s.
+        command_timeout = "45s"
+
+        # Exit with an error when accessing a struct or map field/key that does not
+        # exist. The default behavior will print "<no value>" when accessing a field
+        # that does not exist. It is highly recommended you set this to "true" when
+        # retrieving secrets from Vault.
+        error_on_missing_key = false
+
+        # This is the permission to render the file. If this option is left
+        # unspecified, Consul Template will attempt to match the permissions of the
+        # file that already exists at the destination path. If no file exists at that
+        # path, the permissions are 0644.
+        perms = 0550
+
+        # This option backs up the previously rendered template at the destination
+        # path before writing a new one. It keeps exactly one backup. This option is
+        # useful for preventing accidental changes to the data without having a
+        # rollback strategy.
+        backup = true
+
+        # These are the delimiters to use in the template. The default is "{{" and
+        # "}}", but for some templates, it may be easier to use a different delimiter
+        # that does not conflict with the output file itself.
+        left_delimiter  = "{{"
+        right_delimiter = "}}"
+
+        # This is the `minimum(:maximum)` to wait before rendering a new template to
+        # disk and triggering a command, separated by a colon (`:`). If the optional
+        # maximum value is omitted, it is assumed to be 4x the required minimum value.
+        # This is a numeric time with a unit suffix ("5s"). There is no default value.
+        # The wait value for a template takes precedence over any globally-configured
+        # wait.
+        wait {
+          min = "2s"
+          max = "10s"
+        }
+      }
+    CONF
+    it 'creates elasticsearch_hosts.hcl in the consul-template template directory' do
+      expect(chef_run).to create_file('/etc/consul-template.d/conf/elasticsearch_hosts.hcl')
+        .with_content(consul_template_elasticsearch_hosts_content)
         .with(
           group: 'root',
           owner: 'root',
