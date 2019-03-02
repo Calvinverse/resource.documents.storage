027fe74add153c1c421dc68a1f66c40fe6ee2e3e
Petrik van der Velde <petrikvandervelde@gmail.com>
2019-03-03 11:08:15 +1300

    Update to the latest cookbook
    references #!

--- src/cookbooks/resource_documents_storage/metadata.rb
+++ src/cookbooks/resource_documents_storage/metadata.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 chef_version '>= 12.5' if respond_to?(:chef_version)
-description 'Environment cookbook that configures a Linux server as a InfluxDB server'
+description 'Environment cookbook that configures a Linux server as an Elasticsearch server'
 issues_url '${ProductUrl}/issues' if respond_to?(:issues_url)
 license 'Apache-2.0'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
@@ -13,7 +13,7 @@ version '${VersionSemantic}'
 
 supports 'ubuntu', '>= 16.04'
 
-depends 'elasticsearch', '= 4.0.1'
+depends 'elasticsearch', '= 4.0.5'
 depends 'firewall', '= 2.6.3'
 depends 'java_se', '= 8.202.0'
 depends 'poise-service', '= 1.5.2'
